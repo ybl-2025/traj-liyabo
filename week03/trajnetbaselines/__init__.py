21104d5f9ea2bcf5bc63cc7a01e0fdea0240257e
ybl-2025 <2250656@tongji.edu.cn>
2026-09-23 13:11:34 +0800

    Delete week03 directory

--- week03/trajnetbaselines/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = '0.1.0'
-
-from . import augmentation
-from . import lstm
-from . import sgan
-# Classroom patch: classical baselines are optional; import explicitly if needed.
-from . import vae
